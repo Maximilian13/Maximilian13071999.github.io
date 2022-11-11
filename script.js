6c42dd003c885ca826c1be7deb851fe6732412e7
Maximilian13 <maxim.myagchilo@gmail.com>
2022-11-11 09:04:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,56 @@ let tg = window.Telegram.WebApp
 
 tg.expand()
 
-let btn = document.querySelector("#click")
-btn.onclick = () => {
-    alert("Нажатие")
+tg.MainButton.textColor = "#FFFFFF"
+tg.MainButton.color = "#FF8979"
+
+let item = ""
+
+let btn1 = document.querySelector("#btn1")
+let btn2 = document.querySelector("#btn2")
+let btn3 = document.querySelector("#btn3")
+let btn4 = document.querySelector("#btn4")
+
+btn1.onclick = () => {
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide()
     } else {
-        tg.MainButton.setText("Вы выбрали кота!")
+        tg.MainButton.setText("Вы выбрали морковь!")
+        item = "морковь"
         tg.MainButton.show()
     }
 }
+
+btn2.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("Вы выбрали картофель!")
+        item = "картофель"
+        tg.MainButton.show()
+    }
+}
+
+btn3.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("Вы выбрали огурец!")
+        item = "огурец"
+        tg.MainButton.show()
+    }
+}
+
+btn4.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("Вы выбрали помидор!")
+        item = "помидор"
+        tg.MainButton.show()
+    }
+}
+
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    tg.sendData(item)
+})
